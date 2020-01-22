5db5c03b3445ea96c423e94eded8c6f99b113a53
Alfonso Graziano <alfonso.lokk3d@gmail.com>
2020-01-22 20:57:35 +0100

    Update vari

--- index.js
+++ index.js
@@ -84,7 +84,13 @@ app.use("/api/codes", codesRouter);
 const envRouter = require("./routes/env");
 app.use("/api/envTag", envRouter);
 
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(_dirname, "client", "build", "index.html"))
+    })
+}
 
 app.listen(port, ()=>{
     console.log('Server running... on port: ' + port);
